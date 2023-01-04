178d5ca6e3805ade4c49943ecb54d948b0aa7bfa
Marco Hernandez <marco7245@hotmail.com>
2023-01-03 22:55:34 -0600

    Eliminar - Editar Añadidos

--- js/main.js
+++ js/main.js
@@ -1,49 +1,55 @@
 //Metodo que permite guarda informacion en el localStorage
 
 function guardar() {
-  contacto = JSON.parse(localStorage.getItem("listItem")) ?? [];
+	contacto = JSON.parse(localStorage.getItem("listItem")) ?? [];
 
-  let id;
-  contacto.length != 0 ? contacto.findLast((item) => (id = item.id)) : (id = 0);
+	let id;
+	contacto.length != 0 ? contacto.findLast((item) => (id = item.id)) : (id = 0);
 
-  if (document.getElementById("id").value) {
-    contacto.forEach((value) => {
-      if (document.getElementById("id").value == value.id) {
-        (value.name = document.getElementById("name").value),
-          (value.age = document.getElementById("age").value),
-          (value.address = document.getElementById("address").value),
-          (value.phone = document.getElementById("phone").value);
-      }
-    });
+	if (document.getElementById("id").value) {
+		contacto.forEach((value) => {
 
-    document.getElementById("id").value = "";
-  } else {
-    let item = {
-      id: id + 1,
-      name: document.getElementById("name").value,
-      age: document.getElementById("age").value,
-      address: document.getElementById("address").value,
-      phone: document.getElementById("phone").value,
-    };
-    contacto.push(item);
-  }
+			if (document.getElementById("id").value == value.id) {
+				(value.name = document.getElementById("name").value),
+					(value.age = document.getElementById("age").value),
+					(value.address = document.getElementById("address").value),
+					(value.phone = document.getElementById("phone").value);
+			}
+		});
 
-  localStorage.setItem("listItem", JSON.stringify(contacto));
+		document.getElementById("id").value = "";
+	} else {
+		let aux = ("" == document.getElementById("name").value)
+		if (aux == true) {
+			console.log("Hola")
+		} else {
+			let item = {
+				id: id + 1,
+				name: document.getElementById("name").value,
+				age: document.getElementById("age").value,
+				address: document.getElementById("address").value,
+				phone: document.getElementById("phone").value,
+			};
+			contacto.push(item);
+		}
+	}
 
-  mostrar();
+	localStorage.setItem("listItem", JSON.stringify(contacto));
 
-  document.getElementById("form").reset();
+	mostrar();
+
+	document.getElementById("form").reset();
 }
 
 // Carga los archivos guardados en el local Storage
 function mostrar() {
-  table.innerHTML = ``;
-  contacto = JSON.parse(localStorage.getItem("listItem")) ?? [];
+	table.innerHTML = ``;
+	contacto = JSON.parse(localStorage.getItem("listItem")) ?? [];
 
-  contacto.forEach(function (value, i) {
-    var table = document.getElementById("table");
+	contacto.forEach(function (value, i) {
+		var table = document.getElementById("table");
 
-    table.innerHTML += `
+		table.innerHTML += `
             <tr>
                 <td>${i + 1}</td>
                 <td>${value.name}</td>
@@ -51,28 +57,44 @@ function mostrar() {
                 <td>${value.address}</td>
                 <td>${value.phone}</td>
                 <td>
-                    <button class="btn btn-sm btn-success" onclick="editar(${value.id})"> <i class="fa fa-edit"></i> </button>
+                    <button class="btn btn-sm btn-success" onclick="buscar(${value.id
+			})"> <i class="fa fa-edit"></i> </button>
                 </td>
                 <td>
-                    <button class="btn btn-sm btn-danger" onclick="eliminar(${value.id})"> <i class="fa fa-trash"> </i> </button>
+                    <button class="btn btn-sm btn-danger" onclick="eliminar(${value.id
+			})"> <i class="fa fa-trash"> </i> </button>
                 </td>
             </tr>`;
-  });
+	});
 }
 
 function limpiar() {
-  document.getElementById("form").reset();
-  document.getElementById("id").value = "";
+	document.getElementById("form").reset();
+	document.getElementById("id").value = "";
 }
 
 function eliminar(id) {
-  contacto = JSON.parse(localStorage.getItem("listItem")) ?? [];
+	contacto = JSON.parse(localStorage.getItem("listItem")) ?? [];
 
-  contacto = contacto.filter(function (value) {
-    return value.id != id;
-  });
+	contacto = contacto.filter(function (value) {
+		return value.id != id;
+	});
 
-  localStorage.setItem("listItem", JSON.stringify(contacto));
+	localStorage.setItem("listItem", JSON.stringify(contacto));
 
-  mostrar();
+	mostrar();
+}
+
+function buscar(id) {
+	contacto = JSON.parse(localStorage.getItem('listItem')) ?? []
+
+	contacto.forEach(function (value) {
+		if (value.id == id) {
+			document.getElementById('id').value = value.id
+			document.getElementById('name').value = value.name
+			document.getElementById('age').value = value.age
+			document.getElementById('address').value = value.address
+			document.getElementById('phone').value = value.phone
+		}
+	})
 }
